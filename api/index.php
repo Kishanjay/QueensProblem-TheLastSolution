bdee640360860f35c4f85df66bcabea8ca9b887f
Kishan Nirghin <kishanjay@live.nl>
2019-10-02 01:21:36 +0200

    Fixes typos on status route

--- api/index.php
+++ api/index.php
@@ -30,8 +30,9 @@ $app->before(function (Request $request) {
 });
 
 $app->get('/', function (Request $request, Silex\Application $app) {
-    $app.json(['success'=>true], 200);
+    return $app->json(['success'=>true], 200);
 });
+
 $app->post('/queensproblemsolver', function (Request $request, Silex\Application $app) {
     $startTime = microtime(true);
 
